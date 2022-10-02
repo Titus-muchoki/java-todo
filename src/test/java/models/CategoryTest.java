b5c45fe99854e5819fb0341ead8f7c600cc4d306
kajela <titoyut56@gmail.com>
2022-10-02 15:21:16 -0400

    add the initial files

--- src/test/java/models/CategoryTest.java
+++ src/test/java/models/CategoryTest.java
@@ -3,4 +3,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CategoryTest {
+    @Test
+    public void find_returnsNullWhenNoTaskFound_null() {
+        assertTrue(Category.find(999) == null);
+    }
 }
