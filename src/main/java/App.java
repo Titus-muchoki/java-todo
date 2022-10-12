b9b14e0cf0b72ae33cde772774f3eed4bfaa5917
kajela <titoyut56@gmail.com>
2022-10-12 06:47:45 -0400

    add the initial files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,10 +12,20 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) { //type “psvm + tab” to autocreate this
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
-        String connectionString = "jdbc:postgresql://localhost:5432/todolist";      //connect to todolist, not todolist_test!
-        Sql2o sql2o = new Sql2o(connectionString, "kajela", "8444");
+//        String connectionString = "jdbc:postgresql://localhost:5432/todolist";      //connect to todolist, not todolist_test!
+//        Sql2o sql2o = new Sql2o(connectionString, "kajela", "8444");
+        String connectionString = "jdbc:postgresql://ec2-35-170-146-54.compute-1.amazonaws.com:5432/d3brs3hiiovcel";
+        Sql2o sql2o = new Sql2o(connectionString, "eefljmbqqnwlnd", "6605188bed79012ba75b85971d424b87c1c90b4bb610b21f5809dab176fa2cc9");
         Sql2oTaskDao taskDao = new Sql2oTaskDao(sql2o);
         Sql2oCategoryDao categoryDao = new Sql2oCategoryDao(sql2o);
 
